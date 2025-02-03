3c0f1eddec13cc8e0f1805bcbccb22d921033d26
masc4424 <arkya22@gmail.com>
2025-02-04 01:42:15 +0530

    change the static files

--- jewlerymanagesys/settings.py
+++ jewlerymanagesys/settings.py
@@ -140,10 +140,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'loginauth', 'static')]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# This is optional if you have custom static folders
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'loginauth', 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
