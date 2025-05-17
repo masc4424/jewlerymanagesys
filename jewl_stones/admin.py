013553b1491c9f3852c112368278137213244aa0
Moupriya Das <moupriya1.das@adamastech.in>
2025-05-17 20:20:31 +0530

    j

--- jewl_stones/admin.py
+++ jewl_stones/admin.py
@@ -1,23 +1,22 @@
 from django.contrib import admin
 from .models import Stone, StoneType, StoneTypeDetail
 
-
 @admin.register(Stone)
 class StoneAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'is_active')
-    list_filter = ('is_active',)
+    list_display = ('name', 'is_active', 'created_by', 'created_at')
     search_fields = ('name',)
+    list_filter = ('is_active', 'created_at')
 
 
 @admin.register(StoneType)
 class StoneTypeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'type_name', 'stone')
-    list_filter = ('stone',)
-    search_fields = ('type_name',)
+    list_display = ('type_name', 'stone', 'created_by', 'created_at')
+    search_fields = ('type_name', 'stone__name')
+    list_filter = ('created_at',)
 
 
 @admin.register(StoneTypeDetail)
 class StoneTypeDetailAdmin(admin.ModelAdmin):
-    list_display = ('id', 'stone', 'stone_type', 'length', 'breadth', 'weight', 'rate')
-    list_filter = ('stone', 'stone_type')
+    list_display = ('stone', 'stone_type', 'length', 'breadth', 'weight', 'rate', 'created_by', 'created_at')
     search_fields = ('stone__name', 'stone_type__type_name')
+    list_filter = ('created_at',)
--- product_inv/admin.py
+++ product_inv/admin.py
@@ -1,31 +1,41 @@
 from django.contrib import admin
 from .models import JewelryType, Model, ModelColor, RawMaterial, RawStones, StoneCount
 
+
+
 @admin.register(JewelryType)
 class JewelryTypeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'unique_id')
+    list_display = ('name', 'unique_id', 'created_by', 'created_at')
     search_fields = ('name', 'unique_id')
+    list_filter = ('created_at',)
+
+
+@admin.register(ModelStatus)
+class ModelStatusAdmin(admin.ModelAdmin):
+    list_display = ('id', 'status', 'created_at')
+    search_fields = ('status',)
+    list_filter = ('created_at',)
 
 
 @admin.register(Model)
 class ModelAdmin(admin.ModelAdmin):
-    list_display = ('model_no', 'length', 'breadth', 'weight', 'jewelry_type')
-    list_filter = ('jewelry_type',)
+    list_display = ('model_no', 'length', 'breadth', 'weight', 'jewelry_type', 'status')
     search_fields = ('model_no',)
+    list_filter = ('jewelry_type', 'status')
 
 
 @admin.register(ModelColor)
 class ModelColorAdmin(admin.ModelAdmin):
     list_display = ('model', 'color')
-    list_filter = ('color',)
     search_fields = ('model__model_no', 'color')
+    list_filter = ('color',)
 
 
 @admin.register(RawMaterial)
 class RawMaterialAdmin(admin.ModelAdmin):
     list_display = ('model', 'metal', 'weight', 'unit')
-    list_filter = ('unit', 'metal')
     search_fields = ('model__model_no', 'metal__name')
+    list_filter = ('unit',)
 
 
 @admin.register(RawStones)
@@ -39,3 +49,9 @@ class StoneCountAdmin(admin.ModelAdmin):
     list_display = ('model', 'stone_type_details', 'count')
     search_fields = ('model__model_no', 'stone_type_details__stone_type__type_name')
 
+
+@admin.register(ModelClient)
+class ModelClientAdmin(admin.ModelAdmin):
+    list_display = ('model', 'client', 'created_by', 'created_at')
+    search_fields = ('model__model_no', 'client__username')
+    list_filter = ('created_at',)
